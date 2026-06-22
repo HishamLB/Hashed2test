4c732ea3b4d9af14d73bc0165b8599c10f7c2423
Hesham <1221101049@student.mmu.edu.my>
2026-06-22 21:00:15 +0800

    Initial tmux script

---
+++ hashed2test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+
+session="hashed2test"
+
+tmux new-session -d -s $session
+
+tmux rename-window -t 0 'Server'
+tmux send-keys -t 'Server' 'zsh' C-m 'clear' C-m
+
+tmux send-keys -t 'Server' "./hashed2nite server --port $2" C-m
+
+tmux send-keys -t 'Server' "$3" C-m
+
+
+tmux new-window -t $session:1 -n 'Clients'
+tmux send-keys -t "$session:1.0" "./hashed2nite client $1" C-m
+
+for ((i=1; i<$3; i++)); do
+    tmux split-window -h -t "$session:1"
+    tmux send-keys "./hashed2nite client $1" C-m
+done
+tmux select-layout -t "$session:1" tiled
+
